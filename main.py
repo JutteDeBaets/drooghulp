526a0bb167b9374e4df1a31261d106b3ab17fe94
Jutte De Baets <Jutte.debaets@hotmail.com>
2026-03-26 01:20:56 +0100

    eerste aanpassing test

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ Main entry point for the Drooghulp project.
 """
 
 def main():
-    print("Welcome to Drooghulp!")
-    # Add your main logic here
+    i=1
+    while i<10:
+     print("Welcome to Drooghulp!")
+     i = i +1
+  # Add your main logic here
 
 if __name__ == "__main__":
     main()
